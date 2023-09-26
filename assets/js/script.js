0248d2590ceef7954ac8b17d296c8c5b521f1b7e
LeandroAvilaTapia <leandro.avila.tapia@gmail.com>
2023-09-26 03:20:55 -0300

    se agregaron los clientes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,197 @@
+
+let clientes = [
+  {
+    "name": "Aceitera Los Cardos",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/pkakvwrml6b4ctuc5ty5.png",
+    "id": 1
+  },
+  {
+    "name": "Adeco agro",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/qywu7f82ajbalxmdesu4.png",
+    "id": 2
+  },
+  {
+    "name": "Agricultores Federados Argentinos",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/yzaquwzwsszgemkv2qig.png",
+    "id": 3
+  },
+  {
+    "name": "AGCO",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/m96nbtxsen94wy2bci3i.png",
+    "id": 4
+  },
+  {
+    "name": "AGD Alimentos Naturales",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/l01hj0sobkmdcwgmzjqj.png",
+    "id": 5
+  },
+  {
+    "name": "Buyanor",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/pxhkdvytbqqjznl6qkhr.png",
+    "id": 6
+  },
+  {
+    "name": "CAPUTO Construcciones y Servicios",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/bydsazgbmxifu8aukrb3.png",
+    "id": 7
+  },
+  {
+    "name": "CICSA",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/byx1vvl6e4qvo0dq6twb.png",
+    "id": 8
+  },
+  {
+    "name": "CINTER",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/triiotpazyc4mpcefycf.png",
+    "id": 9
+  },
+  {
+    "name": "Crucianelli",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/nvf2hm0ta9pv6ufzawxr.png",
+    "id": 10
+  },
+  {
+    "name": "Dacomat",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/gbao0nx1sgglbhtt84rq.png",
+    "id": 11
+  },
+  {
+    "name": "Club Social y Deportivo Defensa y Justicia",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/hammnvtkm6yzqbxcu4oj.png",
+    "id": 12
+  },
+  {
+    "name": "Droguería Sur",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/guvezojxjiukhtocs1xs.png",
+    "id": 13
+  },
+  {
+    "name": "Full-Metal",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/j0pcnuwrbtxcdnuxzi3z.png",
+    "id": 14
+  },
+  {
+    "name": "General Motors",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/cganyap37eo48qicjft2.png",
+    "id": 15
+  },
+  {
+    "name": "Gerdau",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/qwyxydhorvhjcri4sr8p.png",
+    "id": 16
+  },
+  {
+    "name": "Industrias Guidi",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/dqzcyg66hl6vyzru1ez2.png",
+    "id": 17
+  },
+  {
+    "name": "Haras Del Sur",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/mvlrnkxsqrvdctohfq07.png",
+    "id": 18
+  },
+  {
+    "name": "Honda",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/kmkprmzo359ghimuadpy.png",
+    "id": 19
+  },
+  {
+    "name": "John Deere",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/gvquqbvkwdayp3ihc895.png",
+    "id": 20
+  },
+  {
+    "name": "Laminados Industriales",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/o6byqpy1jgouyel5bdbj.png",
+    "id": 21
+  },
+  {
+    "name": "Liliana",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/xlf4lrvtnx9hzht5gg7g.png",
+    "id": 22
+  },
+  {
+    "name": "Lucchetti",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/ionxvl14thiggkdjnqon.png",
+    "id": 23
+  },
+  {
+    "name": "Molinos",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/mzfj5gn4bzydghr07b1a.png",
+    "id": 24
+  },
+  {
+    "name": "Musimundo",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/giziil38kw75klhf0spu.png",
+    "id": 25
+  },
+  {
+    "name": "Naldo",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/pjmkwaywxtvlm0ibl7pl.png",
+    "id": 26
+  },
+  {
+    "name": "Nestlé Purina",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/syijjyy6rftrt8wiugh3.png",
+    "id": 27
+  },
+  {
+    "name": "Renault",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/uds9reonldckamfrwdn9.png",
+    "id": 28
+  },
+  {
+    "name": "Sidersa",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/es2v9asziodw5mxi3s9s.png",
+    "id": 29
+  },
+  {
+    "name": "Simplot",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/pop2libutgy3gtbmck8z.png",
+    "id": 30
+  },
+  {
+    "name": "SRA",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/eas1hfa3bivoxrpv5bgu.png",
+    "id": 31
+  },
+  {
+    "name": "Ternium",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/ttm2bxma1drmow8l0vfa.png",
+    "id": 32
+  },
+  {
+    "name": "Tizi",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/u4fpf4nwk7nf0oc926pk.png",
+    "id": 33
+  },
+  {
+    "name": "Vale",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/bobuqork8m5fdifkxzmj.png",
+    "id": 34
+  },
+  {
+    "name": "Vicentin",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/ffw5tixm1plxseqhyrgy.png",
+    "id": 35
+  },
+  {
+    "name": "Terralagos",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/dj3pvipnmt5w6dfmzqdb.png",
+    "id": 36
+  },
+  {
+    "name": "Whirlpool",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/lw5wr4rxi8upkkoacenl.png",
+    "id": 37
+  },
+  {
+    "name": "Yamaha",
+    "url": "https://res.cloudinary.com/dntf2dwro/image/upload/v1695704723/Lekta/Clientes/u0f24jmjkvhkweyxkhpl.png",
+    "id": 38
+  }
+];
+
 const { createApp } = Vue;
 
 const app = createApp({
@@ -6,6 +200,7 @@ const app = createApp({
       contadorID: 1,
       imagenes: [],
       currentIndex: 0,
+      clientesVue: clientes,
       proyectos: [
         {
           Id: 1,
